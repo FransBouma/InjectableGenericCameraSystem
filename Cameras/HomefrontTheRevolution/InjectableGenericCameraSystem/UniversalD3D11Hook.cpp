89a5a42b8a4ff2d166ceb9c2e9be85fbed41d1a0
Frans Bouma <frans@sd.nl>
2017-07-16 11:18:22 +0200

    Overlay now shows up always. Prototype code still, very ugly!

--- Cameras/HomefrontTheRevolution/InjectableGenericCameraSystem/UniversalD3D11Hook.cpp
+++ Cameras/HomefrontTheRevolution/InjectableGenericCameraSystem/UniversalD3D11Hook.cpp
@@ -17,42 +17,65 @@ typedef void(__stdcall *D3D11ClearRenderTargetViewHook) (ID3D11DeviceContext* pC
 
 ID3D11Device *pDevice = NULL;
 ID3D11DeviceContext *pContext = NULL;
-
-__int64* pSwapChainVtable = NULL;
-__int64* pDeviceContextVTable = NULL;
-
 D3D11PresentHook phookD3D11Present = NULL;
 D3D11DrawIndexedHook phookD3D11DrawIndexed = NULL;
 D3D11ClearRenderTargetViewHook phookD3D11ClearRenderTargetView = NULL;
+static ID3D11RenderTargetView*  g_mainRenderTargetView = NULL;
 
 bool firstTime = true;
+bool tmpSwapChainInitialized = false;
 void* detourBuffer = NULL;
 bool showTestWindow = true;
 
 HRESULT __stdcall hookD3D11Present(IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT Flags)
 {
-	if (firstTime)
+	if (tmpSwapChainInitialized)
 	{
-		if (FAILED(pSwapChain->GetDevice(__uuidof(pDevice), (void**)&pDevice)))
+		if (!(Flags & DXGI_PRESENT_TEST))
 		{
-			IGCS::Console::WriteError("Failed to get device from hooked swapchain");
+			if (firstTime)
+			{
+				if (FAILED(pSwapChain->GetDevice(__uuidof(pDevice), (void**)&pDevice)))
+				{
+					IGCS::Console::WriteError("Failed to get device from hooked swapchain");
+				}
+				else
+				{
+					cout << "pDevice: " << (hex) << (void*)pDevice << endl;
+					pDevice->GetImmediateContext(&pContext);
+				}
+				if (nullptr == pContext)
+				{
+					IGCS::Console::WriteError("Failed to get device context from hooked swapchain");
+				}
+				else
+				{
+					cout << "pContext: " << (hex) << (void*)pContext << endl;
+				}
+
+				firstTime = false;
+				IGCS::Console::WriteLine("Hello from hookD3DPresent");
+				ImGui_ImplDX11_Init(IGCS::Globals::instance().mainWindowHandle(), pDevice, pContext);
+			}
+			DXGI_SWAP_CHAIN_DESC sd;
+			pSwapChain->GetDesc(&sd);
+
+			// Create the render target
+			ID3D11Texture2D* pBackBuffer;
+			D3D11_RENDER_TARGET_VIEW_DESC render_target_view_desc;
+			ZeroMemory(&render_target_view_desc, sizeof(render_target_view_desc));
+			render_target_view_desc.Format = sd.BufferDesc.Format;
+			render_target_view_desc.ViewDimension = D3D11_RTV_DIMENSION_TEXTURE2D;
+			pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
+			pDevice->CreateRenderTargetView(pBackBuffer, &render_target_view_desc, &g_mainRenderTargetView);
+			pContext->OMSetRenderTargets(1, &g_mainRenderTargetView, NULL);
+			pBackBuffer->Release();
+			ImGui_ImplDX11_NewFrame();
+			ImGui::SetNextWindowPos(ImVec2(0, 0), ImGuiSetCond_FirstUseEver);     // Normally user code doesn't need/want to call it because positions are saved in .ini file anyway. Here we just want to make the demo initial state a bit more friendly!
+			ImGui::ShowTestWindow(&showTestWindow);
+			ImGui::Render();
 		}
-		else
-		{
-			pDevice->GetImmediateContext(&pContext);
-		}
-		if(nullptr == pContext)
-		{
-			IGCS::Console::WriteError("Failed to get device context from hooked swapchain");
-		}
-		firstTime = false;
-		IGCS::Console::WriteLine("Hello from hookD3DPresent");
-		ImGui_ImplDX11_Init(IGCS::Globals::instance().mainWindowHandle(), pDevice, pContext);
 	}
-	ImGui_ImplDX11_NewFrame();
-	ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiSetCond_FirstUseEver);     // Normally user code doesn't need/want to call it because positions are saved in .ini file anyway. Here we just want to make the demo initial state a bit more friendly!
-	ImGui::ShowTestWindow(&showTestWindow);
-	ImGui::Render();
 	return phookD3D11Present(pSwapChain, SyncInterval, Flags);
 }
 
@@ -70,9 +93,8 @@ void InitializeHook()
 {
 	IGCS::Console::WriteLine("InitializeHook start");
 	HWND hWnd = IGCS::Globals::instance().mainWindowHandle();
-	IDXGISwapChain* pSwapChain;
 
-	D3D_FEATURE_LEVEL featureLevel = D3D_FEATURE_LEVEL_11_0;
+	D3D_FEATURE_LEVEL featureLevel = D3D_FEATURE_LEVEL_11_1;
 	DXGI_SWAP_CHAIN_DESC swapChainDesc;
 	ZeroMemory(&swapChainDesc, sizeof(swapChainDesc));
 	swapChainDesc.BufferCount = 1;
@@ -85,16 +107,21 @@ void InitializeHook()
 	swapChainDesc.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
 	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
 
+	ID3D11Device *pTmpDevice = NULL;
+	ID3D11DeviceContext *pTmpContext = NULL;
+	IDXGISwapChain* pTmpSwapChain;
 	if (FAILED(D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, NULL, &featureLevel, 1
-		, D3D11_SDK_VERSION, &swapChainDesc, &pSwapChain, &pDevice, NULL, &pContext)))
+		, D3D11_SDK_VERSION, &swapChainDesc, &pTmpSwapChain, &pTmpDevice, NULL, &pTmpContext)))
 	{
 		IGCS::Console::WriteError("Failed to create directX device and swapchain!");
 		return;
 	}
 
-	pSwapChainVtable = (__int64*)pSwapChain;
+	__int64* pSwapChainVtable = NULL;
+	__int64* pDeviceContextVTable = NULL;
+	pSwapChainVtable = (__int64*)pTmpSwapChain;
 	pSwapChainVtable = (__int64*)pSwapChainVtable[0];
-	pDeviceContextVTable = (__int64*)pContext;
+	pDeviceContextVTable = (__int64*)pTmpContext;
 	pDeviceContextVTable = (__int64*)pDeviceContextVTable[0];
 
 	cout << "Present Address : " << hex << (void*)(__int64*)pSwapChainVtable[8] << endl;
@@ -107,8 +134,9 @@ void InitializeHook()
 	{
 		IGCS::Console::WriteError("Enabling of hook failed!");
 	}
-	pDevice->Release();
-	pContext->Release();
-	pSwapChain->Release();
+	pTmpDevice->Release();
+	pTmpContext->Release();
+	pTmpSwapChain->Release();
+	tmpSwapChainInitialized = true;
 	IGCS::Console::WriteLine("InitializeHook end");
 }
