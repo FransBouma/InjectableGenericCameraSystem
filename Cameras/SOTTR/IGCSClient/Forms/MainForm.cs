d1726323278ae9c087b432ce196131049f601709
Frans Bouma <frans@sd.nl>
2020-01-03 14:48:33 +0100

    Added thanks to about, removed 32bit preference for client

--- Cameras/SOTTR/IGCSClient/Forms/MainForm.cs
+++ Cameras/SOTTR/IGCSClient/Forms/MainForm.cs
@@ -146,6 +146,10 @@ Redistribution and use in source and binary forms, with or without modification,
  2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.)";
+
+			_aboutIGCSTextBox.Text += Environment.NewLine + Environment.NewLine + @"Special thanks to: 
+ 1. Crosire, for creating and opening the Reshade sourcecode. It was a great help with grabbing screenshots on DX12!
+ 2. The users of my camera's, you all are a great inspiration to make the best camera tools possible!)";
 		}
 
 		
